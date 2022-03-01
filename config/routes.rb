703a42debaadaef8d60d3cff59f17d09d9f23463
EC2 Default User <ec2-user@ip-172-31-26-50.ap-northeast-1.compute.internal>
2022-03-01 16:27:45 +0000

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # get 'top/index'
   root to: 'articles#index'
   
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
