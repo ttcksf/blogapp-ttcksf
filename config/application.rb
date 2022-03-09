d7617d46a76ba17fdd1cd2e6365af61c16c5a7f5
EC2 Default User <ec2-user@ip-172-31-26-50.ap-northeast-1.compute.internal>
2022-03-09 20:40:49 +0000

    dotenv

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module Kanban
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
-
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
