8327d1b6b507813137f7a32df8fab088162760ce
EC2 Default User <ec2-user@ip-172-31-26-50.ap-northeast-1.compute.internal>
2022-03-10 06:25:27 +0000

    actiontext

---
+++ db/migrate/20220310051215_remove_content_from_articles.rb
@@ -0,0 +1,9 @@
+class RemoveContentFromArticles < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :articles, :content
+  end
+  
+  def down
+    add_column :articles, :content, :text    
+  end
+end
