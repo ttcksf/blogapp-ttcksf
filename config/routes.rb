e4728eebecde36a6bb71013d15b3fbaeaf4f42e6
EC2 Default User <ec2-user@ip-172-31-26-50.ap-northeast-1.compute.internal>
2022-03-14 15:08:27 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'top/index'
   root to: 'articles#index'
+  resource :timeline, only: [:show]
   
   
   resources :articles do
     resources :comments, only: [:new, :create]
     
     resource :like, only: [:create, :destroy]
+  end
+  
+  resources :accounts, only: [:show] do
+    resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
   end
   
   resource :profile, only: [:show, :edit, :update]
